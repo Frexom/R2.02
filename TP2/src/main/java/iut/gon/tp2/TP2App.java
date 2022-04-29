886ea9e49cc4d0d182b9d91a2f56ddc2a22fb9aa
Frexom <hugom2003@sfr.fr>
2022-04-29 10:50:22 +0200

    MenuItems Added first 2 MenuItems and their EventHandler

--- TP2/src/main/java/iut/gon/tp2/TP2App.java
+++ TP2/src/main/java/iut/gon/tp2/TP2App.java
@@ -3,6 +3,7 @@ package iut.gon.tp2;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -15,82 +16,122 @@ import java.io.IOException;
 
 public class TP2App extends Application {
 
-  private BorderPane contenu;
-  private ListView<String> gauche;
-  private ListView<String> droite;
-  private Button versGauche;
-  private Button versDroite;
-  private Button retireTout;
-  private Button ajouteTout;
+	private BorderPane contenu;
+	private ListView<String> gauche;
+	private ListView<String> droite;
+	private Button versGauche;
+	private Button versDroite;
+	private Button retireTout;
+	private Button ajouteTout;
+	private Menu menuFichiers;
+	private Menu menuAide;
+	private MenuItem itemQuitter;
+	private MenuItem itemApropos;
 
-  @Override
-  public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(TP2App.class.getResource("Tp2.fxml"));
-    contenu = fxmlLoader.load();
-    Scene scene = new Scene(contenu);
-    extraitIds(scene);
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(TP2App.class.getResource("Tp2.fxml"));
+		contenu = fxmlLoader.load();
+		Scene scene = new Scene(contenu);
+		extraitIds(scene);
 
-    prepareMenus((MenuBar) scene.lookup("#menus"));
-    prepareListe();
-    prepareBoutons();
-    prepareFermeture(stage);
+		prepareMenus((MenuBar) scene.lookup("#menus"));
+		prepareListe();
+		prepareBoutons();
+		prepareFermeture(stage);
 
-    stage.setTitle("Gestion de groupe");
-    stage.setScene(scene);
-    stage.show();
-  }
+		stage.setTitle("Gestion de groupe");
+		stage.setScene(scene);
+		stage.show();
+	}
 
-  /** Prépare la fenêtre pour demander confirmation avant fermeture */
-  private void prepareFermeture(Stage stage) {
-    stage.setOnCloseRequest(event -> {
-      //TODO confirmer ou consommer l'événement
-    });
-  }
+	/** Prépare la fenêtre pour demander confirmation avant fermeture */
+	private void prepareFermeture(Stage stage) {
+		stage.setOnCloseRequest(event -> {
+			//TODO confirmer ou consommer l'événement
+		});
+	}
 
-  /** Prépare les actions des boutons */
-  private void prepareBoutons() {
-    ajouteTout.setOnAction(this::onAjouteTout);
-    retireTout.setOnAction(this::onRetireTout);
-    // TODO actions des deux boutons centraux
-  }
+	/** Prépare les actions des boutons */
+	private void prepareBoutons() {
+		ajouteTout.setOnAction(this::onAjouteTout);
+		retireTout.setOnAction(this::onRetireTout);
+		// TODO actions des deux boutons centraux
+	}
 
-  /** Ajoute tous les éléments de gauche dans la liste de droite
+	/** Ajoute tous les éléments de gauche dans la liste de droite
    Active le bouton "Retirer tout" et désactive le bouton "Ajouter tout" */
-  private void onAjouteTout(ActionEvent actionEvent) {
-    droite.getItems().addAll(gauche.getItems());
-    gauche.getItems().clear();
-    //TODO active/désactive les boutons
-  }
+	private void onAjouteTout(ActionEvent actionEvent) {
+		droite.getItems().addAll(gauche.getItems());
+		gauche.getItems().clear();
+		//TODO active/désactive les boutons
+	}
 
-  /** Ajoute tous les éléments de droite dans la liste de gauche
+	/** Ajoute tous les éléments de droite dans la liste de gauche
    Active le bouton "Ajouter tout" et désactive le bouton "Retirer tout" */
-  private void onRetireTout(ActionEvent actionEvent) {
-    //TODO
-  }
+	private void onRetireTout(ActionEvent actionEvent) {
+		//TODO
+	}
 
-  /** Prépare les menus et leurs événements */
-  private void prepareMenus(MenuBar menus) {
-    //TODO Remplir la barre de menus
-  }
+	/** Prépare les menus et leurs événements */
+	private void prepareMenus(MenuBar menus) {
 
-  /**
+		this.menuFichiers = new Menu("_Fichiers");
+		this.menuAide = new Menu("_Aide");
+		this.itemQuitter = new MenuItem("Quitter");
+		this.itemApropos = new MenuItem("À propos");
+
+
+
+		itemQuitter.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Platform.exit();
+
+			}
+
+		});
+		
+		itemApropos.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Alert alerte = new Alert(null,"Fermer", ButtonType.CLOSE);
+				alerte.setContentText("J'aime le pain");
+				alerte.setTitle("À propos");
+				alerte.show();
+			}
+
+		});
+		
+		
+		this.menuFichiers.getItems().add(this.itemQuitter);
+		this.menuAide.getItems().add(this.itemApropos);
+		
+		
+
+		menus.getMenus().addAll(this.menuFichiers, this.menuAide);
+	}
+
+	/**
    Remplit la liste de gauche avec des valeurs
    Active le bouton "Ajouter tout"
-   */
-  private void prepareListe() {
-    //TODO active le bouton "Ajouter tout"
-  }
+	 */
+	private void prepareListe() {
+		//TODO active le bouton "Ajouter tout"
+	}
 
-  private void extraitIds(Scene scene) {
-    gauche = (ListView<String>) scene.lookup("#gauche");
-    droite = (ListView<String>) scene.lookup("#droite");
-    versGauche = (Button) scene.lookup("#versGauche");
-    versDroite = (Button) scene.lookup("#versDroite");
-    retireTout = (Button) scene.lookup("#retireTout");
-    ajouteTout = (Button) scene.lookup("#ajouteTout");
-  }
+	private void extraitIds(Scene scene) {
+		gauche = (ListView<String>) scene.lookup("#gauche");
+		droite = (ListView<String>) scene.lookup("#droite");
+		versGauche = (Button) scene.lookup("#versGauche");
+		versDroite = (Button) scene.lookup("#versDroite");
+		retireTout = (Button) scene.lookup("#retireTout");
+		ajouteTout = (Button) scene.lookup("#ajouteTout");
+	}
 
-  public static void main(String[] args) {
-    launch();
-  }
+	public static void main(String[] args) {
+		launch();
+	}
 }
