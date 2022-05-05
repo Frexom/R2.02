1308397567ddca34abc58485c2c963799c8a714a
Frexom <hugom2003@sfr.fr>
2022-05-05 10:29:25 +0200

    Fixed project deleted unnecessary files, fixed imports and all

--- TP3/src/main/java/iut/gon/TP3/GrilleController.java
+++ TP3/src/main/java/iut/gon/TP3/GrilleController.java
@@ -1,8 +1,21 @@
 package iut.gon.TP3;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
 
-public class PrimaryController {
+public class GrilleController implements Initializable {
+	@FXML GridPane grille;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		grille.setStyle("-fx-background-color: red");
+		
+	}
 
+  
 }
