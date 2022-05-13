0cc0c111bbc6339454da5b7c5493c46411c527cf
Frexom <hugom2003@sfr.fr>
2022-05-13 15:14:10 +0200

    Added scores handling

--- TP4/src/main/java/iut/gon/tp4/GrilleController.java
+++ TP4/src/main/java/iut/gon/tp4/GrilleController.java
@@ -1,5 +1,10 @@
 package iut.gon.tp4;
 
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import iut.gon.tp4.Scores.Entree;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,90 +12,110 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Optional;
-import java.util.ResourceBundle;
-
 public class GrilleController implements Initializable {
 
-  private GrilleModel modele;
-  private Scores table;
-  private Scores scores;
+	private GrilleModel modele;
+	private Scores scores;
 
-  public GrilleController(Scores s) {
-    this.modele =  new GrilleModel();
-    this.scores = scores;
-  }
+	public GrilleController(Scores s) {
+		this.modele =  new GrilleModel();
+		this.scores = s;
+	}
 
-  private @FXML GridPane grille;
-  private @FXML HBox statut;
-  private @FXML Label joueur;
+	private @FXML GridPane grille;
+	private @FXML HBox statut;
+	private @FXML Label joueur;
 
-  private Label[][] contenu = new Label[3][3];
+	private Label[][] contenu = new Label[3][3];
 
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    grille.setStyle("-fx-background-color: seashell");
-    for (int l=0; l<3; ++l)
-      for (int c=0; c<3; ++c) {
-        Label label =new Label();
-        label.textProperty().bind(modele.getCase(l,c));
-        grille.add(label, c, l);
-        int lg = l;
-        int col = c;
-        label.setOnMouseClicked(event -> this.joueCase(lg, col));
-        label.setMaxSize(1000,1000);
-        label.setAlignment(Pos.CENTER);
-        label.setFont(Font.font(24));
-      }
-    joueur.textProperty().bind(modele.texteJoueur);
-  }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		grille.setStyle("-fx-background-color: seashell");
+		for (int l=0; l<3; ++l)
+			for (int c=0; c<3; ++c) {
+				Label label =new Label();
+				label.textProperty().bind(modele.getCase(l,c));
+				grille.add(label, c, l);
+				int lg = l;
+				int col = c;
+				label.setOnMouseClicked(event -> this.joueCase(lg, col));
+				label.setMaxSize(1000,1000);
+				label.setAlignment(Pos.CENTER);
+				label.setFont(Font.font(24));
+			}
+		joueur.textProperty().bind(modele.texteJoueur);
+	}
 
-  public void joueCase(int lg, int col) {
-    if (modele.estFinie()) return;
-    try {
-      modele.joueCase(lg, col);
-    } catch (IllegalStateException ex) {
-      new Alert(Alert.AlertType.ERROR,ex.getMessage()).showAndWait();
-      return;
-    }
-    if (modele.estGagne(modele.JOUEUR_X))
-      onGagne(modele.JOUEUR_X);
-    else if (modele.estGagne(modele.JOUEUR_O))
-      onGagne(modele.JOUEUR_O);
-    else if (modele.estFinie())
-      onGagne(null);
-  }
+	public void joueCase(int lg, int col) {
+		if (modele.estFinie()) return;
+		try {
+			modele.joueCase(lg, col);
+		} catch (IllegalStateException ex) {
+			new Alert(Alert.AlertType.ERROR,ex.getMessage()).showAndWait();
+			return;
+		}
+		if (modele.estGagne(modele.JOUEUR_X))
+			onGagne(modele.JOUEUR_X);
+		else if (modele.estGagne(modele.JOUEUR_O))
+			onGagne(modele.JOUEUR_O);
+		else if (modele.estFinie())
+			onGagne(null);
+	}
 
-  private void onGagne(String joueur) {
-    //TODO demander le nom du joueur
-    //TODO modifier scores
-    //TODO appeler la table des scores
-  }
+	private void onGagne(String joueur) {
+		if(joueur != null) {
+			TextInputDialog win = new TextInputDialog(joueur);
+			win.setTitle("Congratulations!");
+			win.setHeaderText("Enter your name : ");
+			win.setContentText("Name");
+			Optional<String> name = win.showAndWait();
+			if(!name.isEmpty()) {
+				this.scores.ajouteVictoire(name.get());
+			}
+			else {
+				this.scores.ajouteVictoire("joueur " + joueur);
+			}
+			String s = "";
+			for(Entree e : this.scores.getScores()) {
+				s += e.getJoueur() + " : " + e.getVictoires() + "\n";
+			}
+			s += "Nulles : " + this.scores.getNulles();
+			
+			Alert displayScores = new Alert(AlertType.NONE);
+			displayScores.setContentText(s);
+			displayScores.setHeaderText("Table des scores : ");
+			displayScores.getDialogPane().getButtonTypes().add(ButtonType.OK);
+			displayScores.showAndWait();
+		}
+		else {
+			this.scores.ajouteNulle();
+		}
+	}
 
-  @FXML
-  public void onMenuNouvelle(ActionEvent evt) {
-    modele.nouvellePartie();
-  }
-  @FXML
-  public void onMenuTable(ActionEvent evt) {
-	  FXMLLoader fxmlLoader = new FXMLLoader(Morpion.class.getResource("table.fxml"));
-	  TableController tableC = fxmlLoader.getController();
-	  tableC.setScores(this.scores);
-	  
-	  //this.grille.getScene().setRoot();
-  }
+	@FXML
+	public void onMenuNouvelle(ActionEvent evt) {
+		modele.nouvellePartie();
+	}
+	@FXML
+	public void onMenuTable(ActionEvent evt) {
+		FXMLLoader fxmlLoader = new FXMLLoader(Morpion.class.getResource("table.fxml"));
+		TableController tableC = fxmlLoader.getController();
+		tableC.setScores(this.scores);
 
-  @FXML
-  public void onMenuQuitter(ActionEvent evt) {
-    Platform.exit();
-  }
+		//this.grille.getScene().setRoot();
+	}
+
+	@FXML
+	public void onMenuQuitter(ActionEvent evt) {
+		Platform.exit();
+	}
 }
--- TP4/src/main/java/iut/gon/tp4/Scores.java
+++ TP4/src/main/java/iut/gon/tp4/Scores.java
@@ -7,64 +7,63 @@ import java.util.ArrayList;
 import java.util.ListIterator;
 
 public class Scores {
-  public class Entree {
-    private String joueur;
-    private int victoires;
+	public class Entree {
+		private String joueur;
+		private int victoires;
 
-    public Entree(String joueur, int victoires) {
-      this.joueur = joueur;
-      this.victoires = victoires;
-    }
+		public Entree(String joueur, int victoires) {
+			this.joueur = joueur;
+			this.victoires = victoires;
+		}
 
-    public String getJoueur() {
-      return joueur;
-    }
+		public String getJoueur() {
+			return joueur;
+		}
 
-    public int getVictoires() {
-      return victoires;
-    }
+		public int getVictoires() {
+			return victoires;
+		}
 
-    public void ajouteVictoire() {
-      ++victoires;
-    }
-  }
-  private ObservableList<Entree> scores;
-  private int nulles;
+		public void ajouteVictoire() {
+			++victoires;
+		}
+	}
+	private ObservableList<Entree> scores;
+	private int nulles;
 
-  public Scores() {
-    scores = FXCollections.observableList(new ArrayList<>());
-    nulles = 0;
-    scores.add(new Entree("Laurent",42));
-  }
+	public Scores() {
+		scores = FXCollections.observableList(new ArrayList<>());
+		nulles = 0;
+	}
 
-  public void ajouteVictoire(String joueur) {
-    for (ListIterator<Entree> iterator = scores.listIterator(); iterator.hasNext(); ) {
-      Entree e = iterator.next();
-      if (e.joueur.equals(joueur)) {
-        e.ajouteVictoire();
-        iterator.set(e); // Pour modifier la liste
-        return;
-      }
-    }
-    // Si on arrive là, c'est un nouveau gagnant !
-    scores.add(new Entree(joueur, 1));
-  }
+	public void ajouteVictoire(String joueur) {
+		for (ListIterator<Entree> iterator = scores.listIterator(); iterator.hasNext(); ) {
+			Entree e = iterator.next();
+			if (e.joueur.equals(joueur)) {
+				e.ajouteVictoire();
+				iterator.set(e); // Pour modifier la liste
+				return;
+			}
+		}
+		// Si on arrive là, c'est un nouveau gagnant !
+		scores.add(new Entree(joueur, 1));
+	}
 
-  public void ajouteNulle() {
-    nulles++;
-  }
+	public void ajouteNulle() {
+		nulles++;
+	}
 
-  public ObservableList<Entree> getScores() {
-    return scores;
-  }
+	public ObservableList<Entree> getScores() {
+		return scores;
+	}
 
-  public int getNulles() {
-    return nulles;
-  }
+	public int getNulles() {
+		return nulles;
+	}
 
-  public void reinitialiser() {
-    scores.clear();
-    nulles = 0;
-  }
+	public void reinitialiser() {
+		scores.clear();
+		nulles = 0;
+	}
 
 }
