5f99026197cce58561f31ce1a83c1ad429547aaa
Frexom <hugom2003@sfr.fr>
2022-05-17 12:30:20 +0200

    created initGrille method

--- TP5/src/main/java/fr/unicaen/iut/tp5/DemineurController.java
+++ TP5/src/main/java/fr/unicaen/iut/tp5/DemineurController.java
@@ -10,7 +10,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class DemineurController implements Initializable {
 
@@ -29,10 +31,25 @@ public class DemineurController implements Initializable {
 
 	}
 	
-	public void initGrille(int diff1, int diff2, int diff3) {
+	public void initGrille(String userData) {
 		this.gridpane.getRowConstraints().clear();
 		this.gridpane.getColumnConstraints().clear();
-		this.gridpane.getChildren().clear();
+		int[] d = ModeleDemineur.parseUserData(userData);
+		ModeleDemineur m = new ModeleDemineur(d[0], d[1], d[2]);
+		
+	     for (int i = 0; i < d[0]; i++) {
+	         ColumnConstraints column = new ColumnConstraints(100);
+	         column.setMaxWidth(32);
+	         column.setMinWidth(32);
+	         column.setPrefWidth(32);
+	         gridpane.getColumnConstraints().add(column);
+	         
+	         RowConstraints row = new RowConstraints(100);
+	         row.setMaxHeight(32);
+	         row.setMinHeight(32);
+	         row.setPrefHeight(32);
+	         gridpane.getRowConstraints().add(row);
+	     }
 	}
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -42,11 +59,7 @@ public class DemineurController implements Initializable {
 
 			@Override
 			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
-				String diff = (String) newValue.getUserData();
-				int diff1 = Integer.valueOf(diff.substring(0,1));
-				int diff2 = Integer.valueOf(diff.substring(3,4));
-				int diff3 = Integer.parseInt(diff.substring(6));
-				this.initGrille(diff1, diff2, diff3);
+				initGrille((String) newValue.getUserData());
 
 			}
 		});
