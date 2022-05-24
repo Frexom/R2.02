27e1a2ada197a0138f05c9beb02537161613ccf2
Frexom <hugom2003@sfr.fr>
2022-05-24 10:20:25 +0200

    Did start function and little code optimization

--- TP5/src/main/java/fr/unicaen/iut/tp5/DemineurController.java
+++ TP5/src/main/java/fr/unicaen/iut/tp5/DemineurController.java
@@ -7,9 +7,11 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -24,32 +26,32 @@ public class DemineurController implements Initializable {
 	private ToggleGroup difficulty;
 	@FXML
 	private GridPane gridpane;
+	@FXML
+	private AnchorPane anchor;
+	
+	private Scene scene;
 	private ModeleDemineur modele;
+	
 
 
 	public DemineurController() {
-
+		this.modele = new ModeleDemineur(10,10,10);
 	}
 	
 	public void initGrille(String userData) {
 		this.gridpane.getRowConstraints().clear();
 		this.gridpane.getColumnConstraints().clear();
 		int[] d = ModeleDemineur.parseUserData(userData);
-		ModeleDemineur m = new ModeleDemineur(d[0], d[1], d[2]);
+		this.modele.setTaille(d[0], d[1], d[2]);
 		
 	     for (int i = 0; i < d[0]; i++) {
-	         ColumnConstraints column = new ColumnConstraints(100);
-	         column.setMaxWidth(32);
-	         column.setMinWidth(32);
-	         column.setPrefWidth(32);
+	         ColumnConstraints column = new ColumnConstraints(40);
 	         gridpane.getColumnConstraints().add(column);
 	         
-	         RowConstraints row = new RowConstraints(100);
-	         row.setMaxHeight(32);
-	         row.setMinHeight(32);
-	         row.setPrefHeight(32);
+	         RowConstraints row = new RowConstraints(40);
 	         gridpane.getRowConstraints().add(row);
 	     }
+	     
 	}
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -63,6 +65,10 @@ public class DemineurController implements Initializable {
 
 			}
 		});
+	}
+	
+	public Scene getScene() {
+		return this.scene;
 	}
 
 
--- TP5/src/main/java/fr/unicaen/iut/tp5/MainDemineur.java
+++ TP5/src/main/java/fr/unicaen/iut/tp5/MainDemineur.java
@@ -1,8 +1,8 @@
 package fr.unicaen.iut.tp5;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainDemineur extends Application{
@@ -12,7 +12,11 @@ public class MainDemineur extends Application{
 	}
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.setScene(new Scene(new Label("Ok !"), 800, 600));
+		FXMLLoader fxmlLoader = new FXMLLoader(MainDemineur.class.getResource("Grille.fxml"));
+		DemineurController control = new DemineurController();
+		fxmlLoader.setController(control);
+		Scene scene = new Scene(fxmlLoader.load());
+		stage.setScene(scene);
 		stage.show();
 	}
 }
